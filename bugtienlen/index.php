70b681c4a56cc0bb2c985ec4ade65e835c6a0b81
thanhquan1306 <80297073+thanhquan1306@users.noreply.github.com>
2021-07-14 13:12:10 +0700

    index.php

---
+++ bugtienlen/index.php
@@ -0,0 +1,248 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags-->
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="Hack game tiến lên miền nam facebook, hack game sâm lốc facebook, bug game">
+    <meta name="author" content="Sauanla">
+    <meta name="keywords" content="Hack game tiến lên miền nam facebook, hack game sâm lốc facebook, bug game, hack">
+    <link rel="canonical" href="https://qlikesub.xyz/" />
+    <link rel="icon" type="image/png"
+        href="./img/favicon-404.png">
+    <!-- Title Page-->
+    <title>Nguyễn Thành Quân|IT Gà Mờ</title>
+
+    <!-- Icons font CSS-->
+    <link href="vendor/mdi-font/css/material-design-iconic-font.min.css" rel="stylesheet" media="all">
+    <link href="vendor/font-awesome-4.7/css/font-awesome.min.css" rel="stylesheet" media="all">
+    <!-- Font special for pages-->
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i"
+        rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+    <!-- Main CSS-->
+    <link href="css/main.css" rel="stylesheet" media="all">
+</head>
+
+<body>
+    <div id="fb-root"></div>
+    <script async defer crossorigin="anonymous"
+        src="https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v10.0&appId=282992526070477&autoLogAppEvents=1"
+        nonce="RDWdciKb"></script>
+    <div class="page-wrapper p-t-45 p-b-50" id="page-sub">
+        <div class="wrapper wrapper--w790">
+            <div class="card card-5">
+                <div class="card-heading">
+                    <h2 class="title">Tool Bug game TLMN|Nguyễn Thành Quân</h2>
+                </div>
+                <div class="card-body" style="text-align:center">
+
+                    <div class="some-content">
+                        <span>Bạn nào nhanh tay thì được, đến lúc nó fix thì thôi nhé. để mình update sau!! </span>
+                        <div>Trạng thái: <span class="badge bg-danger">Fix hết r ae ạ :))) </span> </div>
+
+
+                        <div class="alert alert-danger" role="alert" style="margin-top:  20px;">
+
+                            <b>AE CHÚ Ý</b>
+                            <br>
+                            <b>FIX RỒI AE Ạ :))))) </b>
+                            <br>
+                            <br>
+                            Cập nhật ngày: 13/5/2021
+                            <br>
+                            <br>
+
+
+                            <div class="embed-responsive embed-responsive-16by9">
+                                <video style="width:100%" controls>
+                                    <source
+                                        src="./video/video.mp4"
+                                        type="video/mp4">
+                                </video>
+                            </div>
+                        </div>
+                    </div>
+
+
+                    <button class="btn btn--radius-2 btn--green" id="yesSub"
+                        style="margin-bottom: 10px; margin-right: 15px;">Rồi 🤗</button>
+                    <button class="btn btn--radius-2 btn--red" id="noSub" style="margin-bottom: 10px;">Chưa 🤨</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="page-wrapper p-t-45 p-b-50" id="page-container" style="display: none">
+        <div class="wrapper wrapper--w790">
+            <div class="card card-5">
+                <div class="card-heading" style="text-align: center;">
+                    <h2 class="title">Created by ThanhQuan</h2>
+                    <h5 class="text-center " style="color: #fff; margin-top: 5px;">Các bạn làm được thì tiếc gì 1
+                        follow cho Facebook của mình 😉</h2>
+                        <a class="text-center btn btn-primary" style="margin-top: 10px;"
+                            href="https://www.facebook.com/thanhquan.1306/" target="_blank"> Follow </a>
+                </div>
+                <div class="card-body">
+                    <div class="alert alert-danger text-center" role="alert">
+
+                        <b>AE CHÚ Ý</b>
+                        <br>
+                        <b>FIX RỒI AE Ạ :))))) </b>
+                        <br>
+                        <br>
+                        Cập nhật ngày: 13/5/2021
+                        <br>
+                        <br>
+
+                        <br>
+                        <b>CẬP NHẬT MỚI</b>
+                        <div class="embed-responsive embed-responsive-16by9">
+                            <video style="width:100%" controls>
+                                <source
+                                    src="./video/video.mp4"
+                                    type="video/mp4">
+                            </video>
+                        </div>
+
+                        <br>
+                        <br>
+                        <button class="btn btn--radius-2 btn--green" id="yesOk"
+                            style="margin-bottom: 10px; margin-right: 15px;">Đồng ý 🤗</button>
+                    </div>
+                    <div class="fb-like" data-href="https://sauanla.github.io/bug/" data-width="" data-layout="standard"
+                        data-action="like" data-size="small" data-share="true"></div>
+                    <form id="frmHack" style="display: none;">
+                        <div class="form-row p-t-20">
+                            <label class="label label--block">Bạn muốn bug game gì?</label>
+                            <div class="p-t-15">
+                                <label class="radio-container m-r-55">Tiến lên miền nam
+                                    <input type="radio" checked="checked" name="exist" value="2001381133500545">
+                                    <span class="checkmark"></span>
+                                </label>
+                                <label class="radio-container m-r-55">Sâm lốc
+                                    <input type="radio" name="exist" value="293603471320745">
+                                    <span class="checkmark"></span>
+                                </label>
+                                <label class="radio-container">Phỏm
+                                    <input type="radio" name="exist" value="2618675498204375">
+                                    <span class="checkmark"></span>
+                                </label>
+                            </div>
+                        </div>
+
+
+                        <div class="form-row p-t-20"
+                            style="background: aquamarine;  border-radius: 10px;    padding: 10px;   ">
+                            <label class="label label--block">Cập nhật mới</label>
+                            <div class="p-t-15">
+                                <label class="radio-container">Tiến lên miền nam mới
+                                    <input type="radio" name="exist" value="553431568952213">
+                                    <span class="checkmark"></span>
+                                </label>
+                                <label class="radio-container m-r-55">Sâm lốc mới
+                                    <input type="radio" name="exist" value="1411421055858193">
+                                    <span class="checkmark"></span>
+                                </label>
+                            </div>
+                        </div>
+
+                        <div class="form-row p-t-20">
+                            <label class="label label--block">Loại?</label>
+                            <div class="p-t-15">
+                                <label class="radio-container m-r-55">Quà (1 nick chỉ được 1 lần nhé)
+                                    <input type="radio" checked="checked" name="type" value="gift">
+                                    <span class="checkmark"></span>
+                                </label>
+                                <label class="radio-container">1M (Không chơi 1 ngày thì mới được)
+                                    <input type="radio" name="type" value="noplay1day">
+                                    <span class="checkmark"></span>
+                                </label>
+                            </div>
+                        </div>
+
+                        <div class="form-row m-b-55" id="range">
+                            <div class="name">Phạm vi</div>
+                            <div class="value">
+                                <div class="row row-space">
+                                    <div class="col-2">
+                                        <div class="input-group-desc">
+                                            <input class="input--style-5" type="number" name="startNumber" value="10">
+                                            <label class="label--desc">Bắt đầu</label>
+                                        </div>
+                                    </div>
+                                    <div class="col-2">
+                                        <div class="input-group-desc">
+                                            <input class="input--style-5" type="number" name="endNumber" value="15">
+                                            <label class="label--desc">Kết thúc</label>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="form-row">
+                            <div class="name">Thời gian delay</div>
+                            <div class="value">
+                                <div class="input-group">
+                                    <input class="input--style-5" type="text" name="timeDelay" value="6000">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="form-row">
+                            <div class="name"></div>
+                            <div class="value">
+                                <div class="input-group">
+                                    <div class="text-center" style="text-align: center;">
+                                        <span class="run" style="display: none">Đang chạy: <p class="badge bg-primary"
+                                                id="run-step"></p> </span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="form-row">
+                            <div class="name"></div>
+                            <div class="value">
+                                <div class="input-group">
+                                    <div class="text-center" style="text-align: center;">
+                                        <button class="btn btn--radius-2 btn--red" id="hackTab"
+                                            style="margin-top: 10px;">Chạy code</button>
+                                        <button class="btn btn--radius-2 btn--red" id="getCode"
+                                            style="margin-top: 10px;">lấy mã code</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="form-row" style="padding-top: 20px;display: none;" id="input-code">
+                            <div class="name">Code đây</div>
+                            <div class="value">
+                                <div class="input-group">
+                                    <textarea rows="10" name="codeInject" style="width: 100%;" type="text"
+                                        name="timeDelay" value="6000"></textarea>
+                                </div>
+                            </div>
+                        </div>
+                    </form>
+
+
+                    <div class="comment-fb">
+                        <div class="fb-comments" data-href="https://sauanla.github.io/hack-game" data-width="100%"
+                            data-numposts="5" data-order-by="reverse_time"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Jquery JS-->
+    <script src="vendor/jquery/jquery.min.js"></script>
+
+    <!-- Main JS-->
+    <script src="js/hack.js"></script>
+
+</body>
+
+</html>
+<!-- end document-->
---
+++ bugtienlen/js/hack.js
@@ -0,0 +1,132 @@
+const _0x3719 = ['#page-container', 'textarea[name=codeInject]', 'hide', '#run-step', 'Không\x20hỗ\x20trợ\x20trình\x20duyệt', '#hackTab', 'prop', '/?context_source_id&context_type=SOLO&source=messenger~data_bug~data_bug&payload={\x22type\x22:\x22', 'http://youtube.com/c/Sauanla', '28VGLubw', '#noSub', 'setItem', '\x22,\x22ciao\x22:\x222000000\x22,\x22payload_id\x22:', '145057XWrtka', 'open', 'noplay1day', '#yesSub', '_blank', 'input[name=\x27exist\x27]:checked', '1oeMVFL', 'undefined', 'show', '#getCode', '120794WSErIP', 'input[name=startNumber]', 'text', '13807NcTZfZ', '2FdUMCk', 'fadeToggle', '81HRarJT', 'const\x20_0x2ab4=[\x27watchCounter\x27,\x27Thành\x20công\x27,\x27currentDate\x27,\x27type\x27,\x27onReward\x27,\x27dispatch\x27,\x27Design\x20by:\x20Sauanla.com\x27,\x27log\x27];(function(_0x3f99c9,_0x2ab440){const\x20_0x56beec=function(_0x205fe6){while(--_0x205fe6){_0x3f99c9[\x27push\x27](_0x3f99c9[\x27shift\x27]());}};_0x56beec(++_0x2ab440);}(_0x2ab4,0x16f));const\x20_0x56be=function(_0x3f99c9,_0x2ab440){_0x3f99c9=_0x3f99c9-0x0;let\x20_0x56beec=_0x2ab4[_0x3f99c9];return\x20_0x56beec;};this[_0x56be(\x270x1\x27)]=0x6;n=0x7a120;let\x20t=0x1;while(t<=0x19){this[_0x56be(\x270x5\x27)][_0x56be(\x270x6\x27)]({\x27id\x27:t,\x27type\x27:this[_0x56be(\x270x4\x27)],\x27ciao\x27:n,\x27currentDate\x27:this[_0x56be(\x270x3\x27)],\x27watchCounter\x27:this[\x27watchCounter\x27]});t++;}console[_0x56be(\x270x0\x27)](_0x56be(\x270x2\x27));console[_0x56be(\x270x0\x27)](_0x56be(\x270x7\x27));\x0a\x0a\x20\x20\x20\x20\x20\x20\x20\x20//no\x20decode\x0a\x20\x20\x20\x20\x20\x20\x20\x20let\x20z\x20=\x200;\x0a\x20\x20\x20\x20\x20\x20\x20\x20while(z<20){\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x0a\x20\x20\x20\x20\x20\x20\x20\x20this.onReward.dispatch({\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20id:\x20t,\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20type:\x20this.type,\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20coin:\x20n,\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20currentDate:\x20this.currentDate,\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20watchCounter:\x20this.watchCounter\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20})\x0a\x20\x20\x20\x20\x20\x20\x20\x20z++;\x0a\x20\x20\x20\x20\x20\x20\x20\x20console.log(z);\x0a\x20\x20\x20\x20\x20\x20\x20\x20}\x0a\x20\x20\x20\x20\x20\x20\x20\x20', '325157biohpA', 'preventDefault', 'https://www.facebook.com/instantgames/play/', 'getItem', '#page-sub', '11795JmomJx', 'Được\x201\x20lần\x20thôi,\x20mai\x20quay\x20lại', '.run', 'input[name=\x27type\x27]', 'href', '225406OefMkC', '#range', '142787aaWpVa', 'disabled', 'input[name=\x27type\x27]:checked', 'val'];
+const _0x4d82 = function(_0x8c0d2c, _0x13beb6) {
+    _0x8c0d2c = _0x8c0d2c - 0x139;
+    let _0x371915 = _0x3719[_0x8c0d2c];
+    return _0x371915;
+};
+const _0xe72455 = _0x4d82;
+(function(_0x1fb108, _0x2c99bf) {
+    const _0x24a93b = _0x4d82;
+    while (!![]) {
+        try {
+            const _0x1f1a7c = parseInt(_0x24a93b(0x13b)) + -parseInt(_0x24a93b(0x141)) * parseInt(_0x24a93b(0x14d)) + parseInt(_0x24a93b(0x152)) * -parseInt(_0x24a93b(0x166)) + -parseInt(_0x24a93b(0x157)) + parseInt(_0x24a93b(0x145)) + parseInt(_0x24a93b(0x149)) * -parseInt(_0x24a93b(0x159)) + parseInt(_0x24a93b(0x14b)) * parseInt(_0x24a93b(0x148));
+            if (_0x1f1a7c === _0x2c99bf)
+                break;
+            else
+                _0x1fb108['push'](_0x1fb108['shift']());
+        } catch (_0x2c8b0a) {
+            _0x1fb108['push'](_0x1fb108['shift']());
+        }
+    }
+}(_0x3719, 0x352dd));
+
+function sleep(_0x5574cc) {
+    return new Promise(_0x5f4d02 => setTimeout(_0x5f4d02, _0x5574cc));
+}
+$(_0xe72455(0x155))['change'](function() {
+    const _0x1e758b = _0xe72455,
+        _0x5eb12d = $(_0x1e758b(0x15b))['val']();
+    _0x5eb12d === _0x1e758b(0x13d) ? $(_0x1e758b(0x158))[_0x1e758b(0x15f)]() : $(_0x1e758b(0x158))[_0x1e758b(0x143)]();
+});
+async function hackTab(_0x412991) {
+    const _0xe085a5 = _0xe72455;
+    _0x412991[_0xe085a5(0x14e)](),
+        $(_0xe085a5(0x154))[_0xe085a5(0x143)](),
+        $(_0xe085a5(0x162))[_0xe085a5(0x163)](_0xe085a5(0x15a), !![]);
+    const _0x50a894 = $(_0xe085a5(0x140))['val'](),
+        _0x4dac30 = $(_0xe085a5(0x15b))[_0xe085a5(0x15c)](),
+        _0x1bf4c1 = $(_0xe085a5(0x146))['val'](),
+        _0x3f1ed4 = $('input[name=endNumber]')['val'](),
+        _0x4d29cf = $('input[name=timeDelay]')['val']();
+    if (_0x4dac30 === 'noplay1day') {
+        $(_0xe085a5(0x160))[_0xe085a5(0x147)](_0xe085a5(0x153));
+        let _0x36722e = 'https://www.facebook.com/instantgames/play/' + _0x50a894 + _0xe085a5(0x164) + _0x4dac30 + _0xe085a5(0x13a) + 0x1 + '}';
+        window[_0xe085a5(0x13c)](_0x36722e, '_blank'),
+            await sleep(_0x4d29cf);
+        return;
+    }
+    let _0x3ab11 = null;
+    for (let _0x133870 = _0x1bf4c1; _0x133870 < _0x3f1ed4; _0x133870++) {
+        $(_0xe085a5(0x160))[_0xe085a5(0x147)](_0x133870);
+        let _0x433702 = _0xe085a5(0x14f) + _0x50a894 + '/?context_source_id&context_type=SOLO&source=messenger~data_bug~data_bug&payload={\x22type\x22:\x22' + _0x4dac30 + '\x22,\x22ciao\x22:\x222000000\x22,\x22payload_id\x22:' + _0x133870 + '}';
+        _0x3ab11 = window[_0xe085a5(0x13c)](_0x433702, _0xe085a5(0x13f)),
+            await sleep(_0x4d29cf);
+    }
+}
+
+function initData() {
+    const _0x57e726 = _0xe72455;
+    if (typeof Storage === _0x57e726(0x142)) {
+        alert(_0x57e726(0x161));
+        return;
+    }
+    const _0x27d9db = localStorage[_0x57e726(0x150)]('isSub');
+    !_0x27d9db ? $(_0x57e726(0x13e))[_0x57e726(0x163)](_0x57e726(0x15a), !![]) : $('#yesSub')[_0x57e726(0x163)](_0x57e726(0x15a), ![]);
+}
+$(document)['ready'](function() {
+    const _0x481b0c = _0xe72455;
+    initData(),
+        $(_0x481b0c(0x162))['click'](_0x522b3a => {
+            hackTab(_0x522b3a);
+        }),
+        $(_0x481b0c(0x144))['click'](_0x780df4 => {
+            const _0xb05ad7 = _0x481b0c;
+            $('#input-code')['toggle'](),
+                _0x780df4['preventDefault']();
+            let _0x292206 = _0xb05ad7(0x14c);
+            $(_0xb05ad7(0x15e))[_0xb05ad7(0x15c)](_0x292206);
+        }),
+        $(_0x481b0c(0x13e))['click'](_0x21e59f => {
+            const _0x24a945 = _0x481b0c;
+            $(_0x24a945(0x15d))[_0x24a945(0x14a)](0xc8),
+                $(_0x24a945(0x151))[_0x24a945(0x14a)](0xc8);
+        }),
+        $(_0x481b0c(0x167))['click'](_0x57ded9 => {
+            const _0x4b9e42 = _0x481b0c;
+            localStorage[_0x4b9e42(0x139)]('isSub', !![]),
+                window['location'][_0x4b9e42(0x156)] = _0x4b9e42(0x165);
+        });
+});
+
+function initDataAfter() {
+    if (typeof(Storage) === "undefined") {
+        alert("Không hỗ trợ trình duyệt");
+        return;
+    }
+
+    const yesSub = localStorage.getItem("yesSub");
+    const yesOk = localStorage.getItem("yesOk");
+    if (!yesSub) {
+        $("#page-sub").show();
+        return;
+    } else {
+        $("#page-sub").hide();
+        $("#page-container").show();
+    }
+    if (!yesOk) {
+        $("#page-sub").hide();
+    } else {
+        $("#frmHack").show();
+        $("#yesOk").hide();
+    }
+
+}
+
+$(document).ready(function() {
+    $("#yesOk").click(function() {
+        $("#frmHack").show();
+        $("#yesOk").hide();
+
+        localStorage.setItem("yesOk", true);
+    });
+
+    $('input[type=radio]').change(function() {
+        $('#hackTab').prop('disabled', false);
+    });
+    $('#yesSub').click(function() {
+        localStorage.setItem("yesSub", true);
+    });
+
+    initDataAfter();
+
+});
